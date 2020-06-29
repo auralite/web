ef8558db5507579f2185cec0d10bc5202301678b
Miguel Piedrafita <soy@miguelpiedrafita.com>
2020-06-29 19:28:28 +0200

    Add Fathom

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,29 @@
 import '../scss/app.scss'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as Fathom from 'fathom-client'
 
-export default function MyApp({ Component, pageProps }) {
+function App({ Component, pageProps }) {
+	const router = useRouter()
+
+	useEffect(() => {
+		Fathom.load('KXUEDJON', {
+			includedDomains: ['auralite.io'],
+			url: 'https://koi.auralite.io/script.js',
+		})
+
+		function onRouteChangeComplete() {
+			Fathom.trackPageview()
+		}
+
+		router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+		return () => {
+			router.events.off('routeChangeComplete', onRouteChangeComplete)
+		}
+	}, [])
+
 	return <Component {...pageProps} />
 }
+
+export default App
