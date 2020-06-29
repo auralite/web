c192c99046e4cf958ca74ab3b6d26fd203285296
Miguel Piedrafita <soy@miguelpiedrafita.com>
2020-06-30 00:40:21 +0200

    SSG landing

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,34 +7,34 @@ export default function Home() {
 		<>
 			<Head>
 				<title>Auralite</title>
-                <meta name="description" content="A social network for the future" />
-                <meta name="og:type" content="website" />
-                <meta name="og:url" content="https://auralite.io/" />
-                <meta name="og:title" content="Auralite" />
-                <meta name="og:description" content="A social network for the future" />
-                <meta name="og:image" content="https://auralite.io/img/card.jpg" />
-                <meta name="twitter:card" content="summary_large_image" />
-                <meta name="twitter:site" content="@goauralite" />
-                <meta name="twitter:title" content="Auralite" />
-                <meta name="twitter:description" content="A social network for the future." />
-                <meta name="twitter:image" content="https://auralite.io/img/card.jpg" />
-                <meta name="twitter:creator" content="@m1guelpf" />
-                <link rel="stylesheet" href="/css/app.css" />
-                <link rel="apple-touch-icon" sizes="180x180" href="/img/icons/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/img/icons/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/img/icons/favicon-16x16.png" />
-                <link rel="manifest" href="/img/icons/site.webmanifest" />
-                <link rel="mask-icon" href="/img/icons/safari-pinned-tab.svg" color="#6875f5" />
-                <link rel="shortcut icon" href="/img/icons/favicon.ico" />
-                <meta name="msapplication-TileColor" content="#603cba" />
-                <meta name="msapplication-config" content="/img/icons/browserconfig.xml" />
-                <meta name="theme-color" content="#6875f5" />
+				<meta name="description" content="A social network for the future" />
+				<meta name="og:type" content="website" />
+				<meta name="og:url" content="https://auralite.io/" />
+				<meta name="og:title" content="Auralite" />
+				<meta name="og:description" content="A social network for the future" />
+				<meta name="og:image" content="https://auralite.io/img/card.jpg" />
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta name="twitter:site" content="@goauralite" />
+				<meta name="twitter:title" content="Auralite" />
+				<meta name="twitter:description" content="A social network for the future." />
+				<meta name="twitter:image" content="https://auralite.io/img/card.jpg" />
+				<meta name="twitter:creator" content="@m1guelpf" />
+				<link rel="stylesheet" href="/css/app.css" />
+				<link rel="apple-touch-icon" sizes="180x180" href="/img/icons/apple-touch-icon.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/img/icons/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/img/icons/favicon-16x16.png" />
+				<link rel="manifest" href="/img/icons/site.webmanifest" />
+				<link rel="mask-icon" href="/img/icons/safari-pinned-tab.svg" color="#6875f5" />
+				<link rel="shortcut icon" href="/img/icons/favicon.ico" />
+				<meta name="msapplication-TileColor" content="#603cba" />
+				<meta name="msapplication-config" content="/img/icons/browserconfig.xml" />
+				<meta name="theme-color" content="#6875f5" />
 			</Head>
 
 			<div className="font-sans text-black antialiased leading-tight bg-gray-200">
 				<ConfirmationModal />
 				<div className="relative bg-white overflow-x-hidden">
-					<div role="presentation" className="h-8 bg-top bg-repeat bg-top-pattern"></div>
+					<div role="presentation" className="h-8 bg-top bg-repeat bg-top-pattern" />
 					<div className="max-w-screen-md mx-auto px-8 pt-16 text-xl text-gray-600 leading-normal">
 						<div role="presentation" className="absolute inset-0 hidden sm:block">
 							<div className="absolute top-0 right-0 p-24 translate-r-full">
@@ -255,4 +255,10 @@ export default function Home() {
 			</div>
 		</>
 	)
+}
+
+export async function getStaticProps() {
+	return {
+		props: {},
+	}
 }
