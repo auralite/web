bcb538989826eec96486b304f337fe4bcb0cb4d6
Miguel Piedrafita <soy@miguelpiedrafita.com>
2020-06-30 00:51:36 +0200

    Auth middleware

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,14 @@
-import '../scss/app.scss'
+import '../../src/scss/app.scss'
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import * as Fathom from 'fathom-client'
+import cookies from 'next-cookies'
+import App from 'next/app'
+import redirectTo from '../../src/utils/redirectTo'
 
-function App({ Component, pageProps }) {
+const AuthPaths = ['/_error', '/login']
+
+const MyApp = ({ Component, pageProps }) => {
 	const router = useRouter()
 
 	useEffect(() => {
@@ -26,4 +31,16 @@ function App({ Component, pageProps }) {
 	return <Component {...pageProps} />
 }
 
-export default App
+MyApp.getInitialProps = async ({ Component, ctx }) => {
+	const appProps = await App.getInitialProps({ Component, ctx })
+	const cookie = cookies(ctx)
+
+	if (ctx.pathname === '/') return { ...appProps }
+
+	if (cookie.auralite_token && AuthPaths.includes(ctx.pathname)) redirectTo('/posts', { res: ctx.res, status: 301 })
+	if (!cookie.auralite_token && !AuthPaths.includes(ctx.pathname)) redirectTo('/login', { res: ctx.res, status: 301 })
+
+	return { ...appProps }
+}
+
+export default MyApp
