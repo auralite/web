1f646388c7c6d55f2dc8fc0cd88bc0a969627e75
Miguel Piedrafita <soy@miguelpiedrafita.com>
2020-06-29 04:40:53 +0200

    Add meta tags & icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,28 @@ export default function Home() {
 		<>
 			<Head>
 				<title>Auralite</title>
-				<link rel="icon" href="/favicon.ico" />
+                <meta name="description" content="A social network for the future" />
+                <meta name="og:type" content="website" />
+                <meta name="og:url" content="https://auralite.io/" />
+                <meta name="og:title" content="Auralite" />
+                <meta name="og:description" content="A social network for the future" />
+                <meta name="og:image" content="https://auralite.io/img/card.jpg" />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:site" content="@goauralite" />
+                <meta name="twitter:title" content="Auralite" />
+                <meta name="twitter:description" content="A social network for the future." />
+                <meta name="twitter:image" content="https://auralite.io/img/card.jpg" />
+                <meta name="twitter:creator" content="@m1guelpf" />
+                <link rel="stylesheet" href="/css/app.css" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/img/icons/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/img/icons/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/img/icons/favicon-16x16.png" />
+                <link rel="manifest" href="/img/icons/site.webmanifest" />
+                <link rel="mask-icon" href="/img/icons/safari-pinned-tab.svg" color="#6875f5" />
+                <link rel="shortcut icon" href="/img/icons/favicon.ico" />
+                <meta name="msapplication-TileColor" content="#603cba" />
+                <meta name="msapplication-config" content="/img/icons/browserconfig.xml" />
+                <meta name="theme-color" content="#6875f5" />
 			</Head>
 
 			<div className="font-sans text-black antialiased leading-tight bg-gray-200">
