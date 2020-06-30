feb97febdc998a21f5f9983daf9c9b246d574f42
Miguel Piedrafita <soy@miguelpiedrafita.com>
2020-06-30 05:24:14 +0200

    Make it a PWA

--- next.config.js
+++ next.config.js
@@ -1,29 +1,37 @@
-const withSourceMaps = require('@zeit/next-source-maps')({
-	devtool: 'hidden-source-map',
-})
-
+const withPWA = require('next-pwa')
+const withSourceMaps = require('@zeit/next-source-maps')({ devtool: 'hidden-source-map' })
 const SentryWebpackPlugin = require('@sentry/webpack-plugin')
+
 const { NEXT_PUBLIC_SENTRY_DSN: SENTRY_DSN, SENTRY_ORG, SENTRY_PROJECT, SENTRY_AUTH_TOKEN, NODE_ENV } = process.env
 
 process.env.SENTRY_DSN = SENTRY_DSN
 
-module.exports = withSourceMaps({
-	webpack: (config, options) => {
-		if (!options.isServer) {
-			config.resolve.alias['@sentry/node'] = '@sentry/browser'
-		}
+module.exports = withPWA(
+	withSourceMaps({
+		poweredByHeader: false,
+		pwa: {
+			disable: process.env.NODE_ENV === 'development',
+			register: true,
+			runtimeCaching: require('./cache.config'),
+			dest: 'public',
+		},
+		webpack: (config, options) => {
+			if (!options.isServer) {
+				config.resolve.alias['@sentry/node'] = '@sentry/browser'
+			}
 
-		if (SENTRY_DSN && SENTRY_ORG && SENTRY_PROJECT && SENTRY_AUTH_TOKEN && NODE_ENV === 'production') {
-			config.plugins.push(
-				new SentryWebpackPlugin({
-					include: '.next',
-					ignore: ['node_modules'],
-					urlPrefix: '~/_next',
-					release: options.buildId,
-				})
-			)
-		}
+			if (SENTRY_DSN && SENTRY_ORG && SENTRY_PROJECT && SENTRY_AUTH_TOKEN && NODE_ENV === 'production') {
+				config.plugins.push(
+					new SentryWebpackPlugin({
+						include: '.next',
+						ignore: ['node_modules'],
+						urlPrefix: '~/_next',
+						release: options.buildId,
+					})
+				)
+			}
 
-		return config
-	},
-})
+			return config
+		},
+	})
+)
