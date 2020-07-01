0ab920d0882c1ee6d28f9b25d1140a3dc7d2f3c0
Miguel Piedrafita <soy@miguelpiedrafita.com>
2020-07-01 02:39:34 +0200

    Extract post page to PostPage component

---
+++ src/components/App/PostPage.js
@@ -0,0 +1,36 @@
+import useSWR from 'swr'
+import Client from '../../utils/Client'
+import useTitle from '../../hooks/title'
+import Post from './Post'
+import Compose from './Compose'
+
+const PostPage = ({ postId }) => {
+	const { data: post, mutate } = useSWR(
+		() => `/api/posts/${postId}`,
+		() => Client.post({ postId })
+	)
+
+	const setTitle = useTitle(post && `${post.author.name} (@${post.author_handle}) on Auralite: ${post.content}`)
+
+	const newPost = (reply) => {
+		mutate((post) => {
+			post.replies.concat(reply)
+
+			return post
+		})
+	}
+
+	return (
+		<>
+			{setTitle}
+			<div className="max-w-md sm:max-w-full border-l border-r relative z-0">
+				<Post post={post} shouldLink={false} />
+				<Compose replyTo={post} onPost={newPost} />
+				{post ? post.replies.map((reply) => <Post key={reply.id} post={reply} showReplyIndicator={false} />) : [...Array(3).keys()].map((key) => <Post key={key} />)}
+				<div className="text-center py-4">You've reached the end of Auralite. Now close the tab and do something else.</div>
+			</div>
+		</>
+	)
+}
+
+export default PostPage
--- src/pages/[profile]/posts/[post].js
+++ src/pages/[profile]/posts/[post].js
@@ -1,43 +1,9 @@
 import { usePageLayout } from '../../../components/App/PageLayout'
-import Post from '../../../components/App/Post'
-import Compose from '../../../components/App/Compose'
-import Client from '../../../utils/Client'
-import useSWR, { mutate } from 'swr'
-import useTitle from '../../../hooks/title'
+import PostPage from '../../../components/App/PostPage'
 
-const Show = ({ postId }) => {
-	const { data: post } = useSWR(
-		() => `/api/posts/${postId}`,
-		() => Client.post({ postId })
-	)
+const Show = PostPage
 
-	const setTitle = useTitle(post && `${post.author.name} (@${post.author_handle}) on Auralite: ${post.content}`)
-
-	const newPost = (reply) => {
-		mutate(
-			() => `/api/posts/${postId}`,
-			(post) => {
-				post.replies.concat(reply)
-
-				return post
-			}
-		)
-	}
-
-	return (
-		<>
-			{setTitle}
-			<div className="max-w-md sm:max-w-full border-l border-r relative z-0">
-				<Post post={post} shouldLink={false} />
-				<Compose replyTo={post} onPost={newPost} />
-				{post ? post.replies.map((reply) => <Post key={reply.id} post={reply} showReplyIndicator={false} />) : [...Array(10).keys()].map((key) => <Post key={key} />)}
-				<div className="text-center py-4">You've reached the end of Auralite. Now close the tab and do something else.</div>
-			</div>
-		</>
-	)
-}
-
-Show.getLayout = usePageLayout()
+Show.getLayout = (page) => usePageLayout()(page)
 
 Show.getInitialProps = async ({ query }) => {
 	return { postId: query.post }
