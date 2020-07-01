d73de8b403ed0b0617256c4d28efea11526f325a
Miguel Piedrafita <soy@miguelpiedrafita.com>
2020-07-02 01:24:23 +0200

    Get rid of old notifications modal

--- src/components/App/PageLayout.js
+++ src/components/App/PageLayout.js
@@ -20,7 +20,6 @@ const PageLayout = ({ children }) => {
 	const router = useRouter()
 	const { data: user } = useSWR('/api/user', () => Client.user())
 	const { data: notifications } = useSWR('/api/notifications', () => Client.notifications())
-	const [notificationsOpen, setNotificationsOpen] = useState(false)
 	const [profileNavigationOpen, setProfileNavigationOpen] = useState(false)
 	const [mobileNavigationOpen, setMobileNavigationOpen] = useState(false)
 	const { ref: mobileRef, excludeRef } = useClickOutside(() => {
@@ -32,11 +31,6 @@ const PageLayout = ({ children }) => {
 		if (!profileNavigationOpen) return
 
 		setProfileNavigationOpen(false)
-	})
-	const { ref: notificationRef } = useClickOutside(() => {
-		if (!notificationsOpen) return
-
-		setNotificationsOpen(false)
 	})
 
 	const logout = () => {
@@ -134,13 +128,6 @@ const PageLayout = ({ children }) => {
 								</button>
 								<div className="hidden md:block">
 									<div className="ml-4 flex items-center md:ml-6">
-										<button onClick={() => setNotificationsOpen((state) => !state)} className="p-1 pr-2.5 border-2 border-transparent text-indigo-300 rounded-full hover:text-white relative" aria-label="Notifications">
-											<svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-												<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-											</svg>
-											{notifications && notifications.filter((notification) => notification.unread).length > 0 && <span className="mr-1 absolute top-0 right-0 text-sm">{notifications.filter((notification) => notification.unread).length}</span>}
-										</button>
-
 										<div ref={profileRef} className="ml-3 relative">
 											<div>
 												<button onClick={() => setProfileNavigationOpen((state) => !state)} className="max-w-xs flex items-center text-sm rounded-full text-white focus:outline-none focus:shadow-solid" id="user-menu" aria-label="User menu" aria-haspopup="true">
@@ -169,35 +156,6 @@ const PageLayout = ({ children }) => {
 										</div>
 									</div>
 								</div>
-							</div>
-						</div>
-
-						<div className="fixed inset-0 overflow-hidden pointer-events-none z-40">
-							<div className="absolute inset-0 overflow-hidden">
-								<Transition show={notificationsOpen} enter="ease-in-out duration-500" enterFrom="opacity-0" enterTo="opacity-100" leave="ease-in-out duration-500" leaveFrom="opacity-100" leaveTo="opacity-0">
-									<div className="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity pointer-events-auto" />
-								</Transition>
-								<section ref={notificationRef} className="absolute inset-y-0 right-0 pl-10 max-w-full flex">
-									<Transition show={notificationsOpen} enter="transform transition ease-in-out duration-500 sm:duration-700" enterFrom="translate-x-full" enterTo="translate-x-0" leave="transform transition ease-in-out duration-500 sm:duration-700" leaveFrom="translate-x-0" leaveTo="translate-x-full">
-										<div className="relative w-screen max-w-md pointer-events-auto">
-											<div className="absolute top-0 left-0 -ml-8 pt-4 pr-2 flex sm:-ml-10 sm:pr-4">
-												<button onClick={() => setNotificationsOpen(false)} aria-label="Close panel" className="text-gray-300 hover:text-white transition ease-in-out duration-150">
-													<svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-														<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-													</svg>
-												</button>
-											</div>
-											<div className="h-full flex flex-col bg-white shadow-xl overflow-y-scroll">
-												<header className="space-y-1 py-6 px-4 bg-indigo-700 sm:px-6">
-													<div className="flex items-center justify-between space-x-3">
-														<h2 className="text-lg leading-7 font-medium text-white">Notifications</h2>
-													</div>
-												</header>
-												<div className="relative flex-1">{notifications ? notifications.map((notification, key) => <Notification key={key} {...notification} />) : <span>Loading notifications...</span>}</div>
-											</div>
-										</div>
-									</Transition>
-								</section>
 							</div>
 						</div>
 					</nav>
