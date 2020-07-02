152efbd16cae3b939210f64decadf60f3eb37796
Miguel Piedrafita <soy@miguelpiedrafita.com>
2020-07-02 04:23:17 +0200

    Close navigation on link

--- src/components/App/NavLink.js
+++ src/components/App/NavLink.js
@@ -1,12 +1,14 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
-const NavLink = ({ children, href, as, ...props }) => {
+const NavLink = ({ children, href, as, onClick, ...props }) => {
 	const router = useRouter()
 
 	return (
 		<Link href={href} as={as} {...props}>
-			<a className={`${router.asPath === (as ?? href) ? 'text-white bg-indigo-900' : 'text-indigo-300 hover:text-white hover:bg-indigo-700 focus:text-white'} group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-md focus:outline-none focus:bg-indigo-700 transition ease-in-out duration-150`}>{children}</a>
+			<a onClick={onClick} className={`${router.asPath === (as ?? href) ? 'text-white bg-indigo-900' : 'text-indigo-300 hover:text-white hover:bg-indigo-700 focus:text-white'} group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-md focus:outline-none focus:bg-indigo-700 transition ease-in-out duration-150`}>
+				{children}
+			</a>
 		</Link>
 	)
 }
--- src/components/App/PageLayout.js
+++ src/components/App/PageLayout.js
@@ -8,7 +8,6 @@ import Transition from '../Global/Transition'
 import Head from '../Global/Head'
 import AlertManager from './AlertManager'
 import Avatar from './Avatar'
-import PostModal from './PostModal'
 import LoadLink from './LoadLink'
 import Skeleton from 'react-loading-skeleton'
 import Cookies from 'js-cookie'
@@ -39,7 +38,6 @@ const PageLayout = ({ children, authCheck }) => {
 		<>
 			<Head />
 			<AlertManager>
-				<PostModal />
 				<div className="fixed inset-0 flex z-40 pointer-events-none">
 					<Transition show={mobileNavigationOpen} enter="transition-opacity ease-linear duration-300" enterFrom="opacity-0" enterTo="opacity-100" leave="transition-opacity ease-linear duration-300" leaveFrom="opacity-100" leaveTo="opacity-0">
 						<div className="fixed inset-0 pointer-events-auto">
@@ -61,31 +59,31 @@ const PageLayout = ({ children, authCheck }) => {
 									<span className="ml-3 text-indigo-50 font-semibold text-xl">Auralite</span>
 								</div>
 								<nav className="mt-5 px-2 space-y-1">
-									<NavLink href="/home">
+									<NavLink onClick={() => setMobileNavigationOpen(false)} href="/home">
 										<svg className="mr-4 h-6 w-6 text-indigo-400 group-hover:text-indigo-300 group-focus:text-indigo-300 transition ease-in-out duration-150" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 											<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
 										</svg>
 										Home
 									</NavLink>
-									<NavLink href="/search">
+									<NavLink onClick={() => setMobileNavigationOpen(false)} href="/search">
 										<svg className="mr-4 h-6 w-6 text-indigo-400 group-hover:text-indigo-300 group-focus:text-indigo-300 transition ease-in-out duration-150" fill="currentColor" viewBox="0 0 20 20">
 											<path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
 										</svg>
 										Search
 									</NavLink>
-									<NavLink href="/notifications">
+									<NavLink onClick={() => setMobileNavigationOpen(false)} href="/notifications">
 										<svg className="mr-4 h-6 w-6 text-indigo-400 group-hover:text-indigo-300 group-focus:text-indigo-300 transition ease-in-out duration-150" stroke="currentColor" fill="none" viewBox="0 0 24 24">
 											<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
 										</svg>
 										Notifications
 									</NavLink>
-									<NavLink href="/[profile]" as={`/${user?.profile?.handle}`}>
+									<NavLink onClick={() => setMobileNavigationOpen(false)} href="/[profile]" as={`/${user?.profile?.handle}`}>
 										<svg className="mr-4 h-6 w-6 text-indigo-400 group-hover:text-indigo-300 group-focus:text-indigo-300 transition ease-in-out duration-150" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
 											<path d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
 										</svg>
 										Profile
 									</NavLink>
-									<NavLink href="/settings">
+									<NavLink onClick={() => setMobileNavigationOpen(false)} href="/settings">
 										<svg className="mr-4 h-6 w-6 text-indigo-400 group-hover:text-indigo-300 group-focus:text-indigo-300 transition ease-in-out duration-150" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
 											<path d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
 											<path d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
