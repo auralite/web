3ef5e8e6205eae298efdbf9d20ebd313617bb35e
Miguel Piedrafita <soy@miguelpiedrafita.com>
2020-07-04 01:57:11 +0200

    Fix notification count

--- src/components/App/PageLayout.js
+++ src/components/App/PageLayout.js
@@ -184,10 +184,12 @@ const PageLayout = ({ children, authCheck }) => {
 							</Link>
 							<Link href="/notifications">
 								<a className="p-4 text-indigo-300 relative" aria-label="Notifications">
-									<svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-										<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-									</svg>
-									{notifications && notifications.filter((notification) => notification.unread).length > 0 && <span className="mr-1 absolute top-0 right-0 text-sm">{notifications.filter((notification) => notification.unread).length}</span>}
+									<div className="relative">
+										<svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+											<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+										</svg>
+										{notifications && notifications.filter((notification) => notification.unread).length > 0 && <span className="-mr-1.5 -mt-1 absolute top-0 right-0 text-sm">{notifications.filter((notification) => notification.unread).length}</span>}
+									</div>
 								</a>
 							</Link>
 							<LoadLink deps={user?.profile} href="/[profile]" as={`/${user?.profile?.handle}`}>
