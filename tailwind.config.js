6ac92a6e1141abe8fb2b7df5cfbe9ce127dee07e
Miguel Piedrafita <soy@miguelpiedrafita.com>
2020-07-05 05:19:04 +0200

    Fix padding on devices without a dock

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,9 +18,9 @@ module.exports = {
 				screen: ["'Nunito Sans'", ...defaultTheme.fontFamily.sans],
 			},
 			padding: {
-				'safe-t': 'env(safe-area-inset-top)',
+				'safe-t': 'max(1rem, env(safe-area-inset-top))',
 				'safe-b': 'env(safe-area-inset-bottom)',
-				header: 'calc(env(safe-area-inset-top) + 3rem)',
+				header: 'calc(max(1rem, env(safe-area-inset-top)) + 3rem)',
 			},
 			rotate: {
 				'-2': '-2deg',
