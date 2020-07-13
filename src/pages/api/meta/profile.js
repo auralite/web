66ffeb2274e8426f669602dcbae0e836ef92c371
Miguel Piedrafita <soy@miguelpiedrafita.com>
2020-07-13 02:22:25 +0200

    Automatically generate open graph images for profiles

--- src/pages/api/meta/profile.js
+++ src/pages/api/meta/profile.js
@@ -19,7 +19,7 @@ const getScreenshot = async (handle) => {
 
 	await page.setViewport({ width: 2048, height: 1170 })
 
-	await page.goto(`${process.env.VERCEL_URL}/meta/profile?handle=${handle}`)
+	await page.goto(`https://auralite.io/meta/profile?handle=${handle}`)
 
 	return await page.screenshot({ type: 'jpeg' })
 }
