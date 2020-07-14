f90214153c05023880e0ff669ccbd45532f98362
Miguel Piedrafita <git@miguelpiedrafita.com>
2020-07-15 01:15:37 +0200

    Ensure alerts display over header

--- src/components/App/AlertManager.js
+++ src/components/App/AlertManager.js
@@ -10,7 +10,7 @@ const AlertManager = ({ children }) => {
 		<>
 			<AlertContext.Provider value={{ alerts, setAlerts }}>{children}</AlertContext.Provider>
 			{alerts && (
-				<div className="fixed inset-0 flex items-end justify-center px-4 py-6 pointer-events-none sm:p-6 sm:items-start sm:justify-end">
+				<div className="fixed inset-0 flex items-end justify-center px-4 py-6 pointer-events-none sm:p-6 sm:items-start sm:justify-end z-40">
 					<TransitionGroup component={null}>
 						{alerts.map(({ title, body }, i) => (
 							<Alert key={i} title={title} body={body} />
