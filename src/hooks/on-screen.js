11101c0fc9cab98ee6cade19b87925080df521a2
Miguel Piedrafita <git@miguelpiedrafita.com>
2020-07-15 03:18:36 +0200

    Fix 500 error

--- src/hooks/on-screen.js
+++ src/hooks/on-screen.js
@@ -1,17 +1,22 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 function useOnScreen(ref, rootMargin = '0px') {
 	const [isIntersecting, setIntersecting] = useState(false)
-
-	const observer = new IntersectionObserver(([entry]) => setIntersecting(entry.isIntersecting), { rootMargin })
+	const observer = useRef(null)
 
 	useEffect(() => {
+		observer.current = new IntersectionObserver(([entry]) => setIntersecting(entry.isIntersecting), { rootMargin })
+	}, [])
+
+	useEffect(() => {
+		if (!observer.current) return
+
 		if (ref.current) observer.observe(ref.current)
 
 		return () => {
 			if (ref.current) observer.unobserve(ref.current)
 		}
-	}, [ref])
+	}, [ref, observer])
 
 	return [ref, isIntersecting]
 }
