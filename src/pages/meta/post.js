d46548b77d3542dacdc6c10b03edb4d0ae722646
Miguel Piedrafita <git@miguelpiedrafita.com>
2020-07-18 03:45:25 +0200

    Fix issue with post meta card

--- src/pages/meta/post.js
+++ src/pages/meta/post.js
@@ -48,11 +48,12 @@ const PostMeta = ({ post }) => {
 					<div className="text-4xl leading-normal text-indigo-600 text-center" style={{ fontSize: '3.5vw' }}>
 						{postContent}
 						<div className="mt-4 flex items-center justify-center space-x-6">
-							{post.media.map((_, key) => (
-								<svg key={key} className="w-10 h-10" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
-									<path d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
-								</svg>
-							))}
+							{post.media &&
+								post.media.map((_, key) => (
+									<svg key={key} className="w-10 h-10" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
+										<path d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+									</svg>
+								))}
 						</div>
 					</div>
 				</div>
