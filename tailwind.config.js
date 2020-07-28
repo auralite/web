32041b783069935cba9c98625a8b6deb3c1e7073
Miguel Piedrafita <git@miguelpiedrafita.com>
2020-07-28 05:59:01 +0200

    Fix build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,8 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 const colors = require('@tailwindcss/ui/colors')
-const mdx = require('@mdx-js/mdx')
 
 module.exports = {
-	purge: {
-		mode: 'all',
-		content: ['./src/**/*.{js,mdx}', './next.config.js'],
-		options: {
-			extractors: [
-				{
-					extensions: ['mdx'],
-					extractor: (content) => {
-						content = mdx.sync(content)
-
-						// Capture as liberally as possible, including things like `h-(screen-1.5)`
-						const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
-
-						// Capture classes within other delimiters like .block(class="w-1/2") in Pug
-						const innerMatches = content.match(/[^<>"'`\s.(){}[\]#=%]*[^<>"'`\s.(){}[\]#=%:]/g) || []
-
-						return broadMatches.concat(innerMatches)
-					},
-				},
-			],
-		},
-	},
+	purge: ['./src/**/*.{js,mdx}', './next.config.js'],
 	theme: {
 		extend: {
 			height: {
