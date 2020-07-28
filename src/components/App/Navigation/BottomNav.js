e2fd80a2104f451093e51206929afc38d79f765a
Miguel Piedrafita <git@miguelpiedrafita.com>
2020-07-28 06:22:12 +0200

    Fix avatar & tailwind hook

--- src/components/App/Navigation/BottomNav.js
+++ src/components/App/Navigation/BottomNav.js
@@ -12,20 +12,20 @@ const BottomNav = () => {
 
 	return (
 		<footer className="sm:hidden bg-white flex fixed bottom-0 z-20 w-full pb-safe-b" style={{ boxShadow: '0 -3px 6px 0 rgba(0, 0, 0, 0.05)' }}>
-			<NavLink href="/home">{(isActive) => (isActive ? <HomeSolid className="w-7 h-7 text-gray-600" /> : <HomeOutline className="w-7 h-7 text-gray-400" />)}</NavLink>
-			<NavLink href="/search">{(isActive) => (isActive ? <SearchSolid className="w-7 h-7 text-gray-600" /> : <SearchOutline className="w-7 h-7 text-gray-400" />)}</NavLink>
+			<NavLink href="/home">{(isActive) => (isActive ? <HomeSolid className="w-6 h-6 text-gray-600" /> : <HomeOutline className="w-6 h-6 text-gray-400" />)}</NavLink>
+			<NavLink href="/search">{(isActive) => (isActive ? <SearchSolid className="w-6 h-6 text-gray-600" /> : <SearchOutline className="w-6 h-6 text-gray-400" />)}</NavLink>
 			<NavLink href="/notifications">
 				{(isActive) => (
 					<div className={`relative ${isActive ? 'text-gray-600' : 'text-gray-400'}`}>
-						{isActive ? <BellSolid className="w-7 h-7" /> : <BellOutline className="w-7 h-7" />}
+						{isActive ? <BellSolid className="w-6 h-6" /> : <BellOutline className="w-6 h-6" />}
 						{notifications && notifications.filter((notification) => notification.unread).length > 0 && <span className="-mr-1.5 -mt-1 absolute top-0 right-0 text-sm">{notifications.filter((notification) => notification.unread).length}</span>}
 					</div>
 				)}
 			</NavLink>
 			<NavLink href="/[profile]" as={`/${user?.profile?.handle}`} className="group focus:outline-none">
 				{(isActive) => (
-					<div className={`w-7 h-7 rounded-full text-gray-600 group-focus:shadow-solid ${isActive ? 'shadow-solid' : ''}`}>
-						<Avatar sizeClasses="h-7 w-7" src={user?.profile?.avatar} />
+					<div className={`w-6 h-6 rounded-full text-gray-600 group-focus:shadow-solid ${isActive ? 'shadow-solid' : ''}`}>
+						<Avatar sizeClasses="h-6 w-6" src={user?.profile?.avatar} />
 					</div>
 				)}
 			</NavLink>
--- src/hooks/tailwind.js
+++ src/hooks/tailwind.js
@@ -1,18 +1,19 @@
 import { theme as defaultConfig } from 'tailwindcss/stubs/defaultConfig.stub'
 import { theme as tailwindConfig } from '../../tailwind.config'
+const tailwind = { ...defaultConfig, ...tailwindConfig }
 
 const useTailwind = (classes, type) => {
 	if (!Array.isArray(type)) type = [type]
 	classes = classes.split(' ')
 
-	const tailwind = { ...defaultConfig, ...tailwindConfig }
-
 	return type.map((key, i) => {
 		const twClass = classes[i]
 		const index = [...twClass.match(/^.*-(.*)$/)][1]
 
-		return tailwind[key][index]
+		return tailwind[key](theme)[index]
 	})
 }
+
+const theme = (key) => tailwind[key]
 
 export default useTailwind
