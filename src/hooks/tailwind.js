b204d16b6901797d372d7bb1b6ad65c082feecde
Miguel Piedrafita <git@miguelpiedrafita.com>
2020-07-28 06:33:20 +0200

    Bundle spacing scale directly

--- src/hooks/tailwind.js
+++ src/hooks/tailwind.js
@@ -1,6 +1,24 @@
-import { theme as defaultConfig } from 'tailwindcss/stubs/defaultConfig.stub'
-import { theme as tailwindConfig } from '../../tailwind.config'
-const tailwind = { ...defaultConfig, ...tailwindConfig }
+const scale = {
+	px: '1px',
+	0: '0',
+	1: '0.25rem',
+	2: '0.5rem',
+	3: '0.75rem',
+	4: '1rem',
+	5: '1.25rem',
+	6: '1.5rem',
+	8: '2rem',
+	10: '2.5rem',
+	12: '3rem',
+	16: '4rem',
+	20: '5rem',
+	24: '6rem',
+	32: '8rem',
+	40: '10rem',
+	48: '12rem',
+	56: '14rem',
+	64: '16rem',
+}
 
 const useTailwind = (classes, type) => {
 	if (!Array.isArray(type)) type = [type]
@@ -10,10 +28,10 @@ const useTailwind = (classes, type) => {
 		const twClass = classes[i]
 		const index = [...twClass.match(/^.*-(.*)$/)][1]
 
-		return tailwind[key](theme)[index]
+		if (!['width', 'height'].includes(key)) throw 'This hook only supports spacing variables'
+
+		return scale[index]
 	})
 }
-
-const theme = (key) => tailwind[key]
 
 export default useTailwind
