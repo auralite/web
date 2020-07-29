4febc98280429e28ebd79df88ed2818a5e519bca
Miguel Piedrafita <git@miguelpiedrafita.com>
2020-07-29 05:09:44 +0200

    Persist theme in localStorage

--- src/components/Global/ThemeManager.js
+++ src/components/Global/ThemeManager.js
@@ -2,12 +2,14 @@ import { useState, useEffect } from 'react'
 import ThemeContext from '@/context/theme'
 
 const ThemeManager = ({ children }) => {
-	const prefersDark = typeof window !== 'undefined' && window?.matchMedia('(prefers-color-scheme: dark)')?.matches
+	const prefersDark = typeof window !== 'undefined' && localStorage.getItem('theme') !== 'light' && window?.matchMedia('(prefers-color-scheme: dark)')?.matches
 
 	const [theme, setTheme] = useState(prefersDark ? 'dark' : 'light')
 
 	useEffect(() => {
 		document.body.classList.add('dark:bg-gray-900', 'sm:dark:bg-gray-700')
+
+		localStorage.setItem('theme', theme)
 
 		if (theme === 'dark') {
 			document.documentElement.classList.add('scheme-dark')
