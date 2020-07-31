f7a4f052fc2e1a98352252b9456cc0132943d46e
Miguel Piedrafita <git@miguelpiedrafita.com>
2020-07-31 17:13:38 +0200

    Create ClientOnly component to prevent SSR issues

---
+++ src/components/App/ClientOnly.js
@@ -0,0 +1,19 @@
+const { useState, useEffect } = require('react')
+
+const ClientOnly = ({ children }) => {
+	if (!isSSR()) return null
+
+	return children
+}
+
+export const isSSR = () => {
+	const [hasMounted, setHasMounted] = useState(false)
+
+	useEffect(() => {
+		setHasMounted(true)
+	}, [])
+
+	return hasMounted
+}
+
+export default ClientOnly
