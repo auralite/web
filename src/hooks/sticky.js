1c92abcbb501f4ef8048e97fce4241dfec4550a1
Miguel Piedrafita <git@miguelpiedrafita.com>
2020-07-31 17:14:25 +0200

    Create stickystate hook

---
+++ src/hooks/sticky.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react'
+
+const useStickyState = (defaultValue, key) => {
+	if (typeof window === 'undefined') {
+		return useState(defaultValue)
+	}
+
+	const [value, setValue] = useState(() => {
+		const stickyValue = window.localStorage.getItem(key)
+		return stickyValue !== null ? JSON.parse(stickyValue) : defaultValue
+	})
+
+	useEffect(() => {
+		window.localStorage.setItem(key, JSON.stringify(value))
+	}, [key, value])
+
+	return [value, setValue]
+}
+
+export default useStickyState
