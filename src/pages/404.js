7a69aa82a357961db22ddcc354b53f5dbcd52dec
Miguel Piedrafita <git@miguelpiedrafita.com>
2020-08-01 20:50:31 +0200

    Improve 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,27 @@
-import Error from 'next/error'
+import React, { useEffect } from 'react'
+import useMousePosition from '@/hooks/mouse'
+import Link from 'next/link'
+import { ThemeBubble } from '@/components/Global/ThemeManager'
+import { ArrowLeftOutline } from '@/components/App/Icon'
 
-const Error404 = () => <Error statusCode={404} />
+const NotFoundPage = () => {
+	return (
+		<>
+			<div className="h-screen flex items-center justify-center text-center dark:text-gray-300 dark:dark-bg">
+				<div>
+					<h1 className="font-black font-screen text-5xl sm:text-6xl">Not Found</h1>
+					<p className="text-xl sm:text-2xl">Sorry, there is nothing at this URL.</p>
+					<Link href="/">
+						<a className="flex items-center sm:justify-center mt-3">
+							<ArrowLeftOutline className="w-6 h-6 mr-2" />
+							<span className="text-xl">Go back home</span>
+						</a>
+					</Link>
+				</div>
+			</div>
+			<ThemeBubble />
+		</>
+	)
+}
 
-export default Error404
+export default NotFoundPage
