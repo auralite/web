ced1fa45fd3eeb88fa25d9ff798c1b1e8e1a1b1e
Miguel Piedrafita <git@miguelpiedrafita.com>
2020-08-02 16:29:59 +0200

    Run middleware on navigation too

--- src/components/Global/BaseLayout.js
+++ src/components/Global/BaseLayout.js
@@ -2,11 +2,14 @@ import AlertManager from '../App/AlertManager'
 import { useEffect } from 'react'
 import Pipeline from 'pipeline-js'
 import ThemeManager from './ThemeManager'
+import { useRouter } from 'next/router'
 
 const BaseLayout = ({ children, middleware }) => {
+	const router = useRouter()
+
 	useEffect(() => {
 		new Pipeline(middleware).process()
-	}, [])
+	}, [router.pathname])
 
 	return (
 		<ThemeManager>
